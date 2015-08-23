6bd6737bcf3a5d3b1c751641145a6ec02e1c5e36
Zach Rickert <zachrickert@gmail.com>
2015-08-23 08:55:38 -0700

    initial commit

---
+++ convert.py
@@ -0,0 +1,10 @@
+#convert.py
+# This program will convert celsius to fahrenheit
+
+def main():
+    celsius = input("What is the temprature (celsius)? ")
+    fahrenheit = 9.0/5.0 * celsius +32.0
+
+    print("The temprature is " + str(fahrenheit) + "oF")
+
+main()
