26aac24a5e19808333555d882773232631f99eee
Zach Rickert <zachrickert@gmail.com>
2015-08-25 16:31:21 -0700

    add F to C conversion.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # convert
 
-This program will convert Celsius to Fahrenheit
+This program will convert one measurement to another
+
+1 = Celsius to Fahrenheit
+2 = Fahrenheit to Celsius
--- convert.py
+++ convert.py
@@ -2,10 +2,19 @@
 # This program will convert celsius to fahrenheit
 
 def main():
-    print ("This program will convert a Celsius temprature to Fahrenheit.")
-    celsius = input("What is the temprature (celsius)? ")
-    fahrenheit = 9.0/5.0 * celsius +32.0
+    print ("This program will convert measurements")
+    print ("1) Celsius to Fahrenheit")
+    print ("2) Fahrenheit to Celsius")
+    conversion_type = input("Enter conversion type: ")
 
-    print("The temprature is " + str(fahrenheit) + "oF")
+    if conversion_type == 1:
+        celsius = input("What is the temprature (celsius)? ")
+        fahrenheit = 9.0/5.0 * celsius + 32.0
+        print("The temprature is " + str(fahrenheit) + "oF")
+
+    if conversion_type == 2:
+        fahrenheit = input("What is the temprature (fahrenheit)? ")
+        celsius = 5.0 * (fahrenheit - 32.0) / 9.0
+        print("The temprature is " + str(celsius) + "oFC")
 
 main()
