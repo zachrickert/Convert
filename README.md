e82f81000fc9a70a268dfb454d7193201649e242
Zach Rickert <zachrickert@gmail.com>
2015-08-25 10:37:45 -0700

    made the program loop through 5 times.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # convert
 
 This program will convert Celsius to Fahrenheit
+this will loop through the calculation 5 times.
+
--- convert.py
+++ convert.py
@@ -3,9 +3,11 @@
 
 def main():
     print ("This program will convert a Celsius temprature to Fahrenheit.")
-    celsius = input("What is the temprature (celsius)? ")
-    fahrenheit = 9.0/5.0 * celsius +32.0
+    print ("The program will loop 5 times.")
 
-    print("The temprature is " + str(fahrenheit) + "oF")
+    for i in range(5):
+        celsius = input("What is the temprature (celsius)? ")
+        fahrenheit = 9.0/5.0 * celsius +32.0
+        print("The temprature is " + str(fahrenheit) + "oF")
 
 main()
