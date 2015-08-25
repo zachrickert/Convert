0b0178f17e6583c71110ed828aa579a5dc1b5a5a
Zach Rickert <zachrickert@gmail.com>
2015-08-25 10:14:27 -0700

    added an introduction to the program.

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@
 # This program will convert celsius to fahrenheit
 
 def main():
+    print ("This program will convert a Celsius temprature to Fahrenheit.")
     celsius = input("What is the temprature (celsius)? ")
     fahrenheit = 9.0/5.0 * celsius +32.0
 
